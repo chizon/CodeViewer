8d37ac5415f406eded18b4847ea352773c89dfbb
wanjiaju3108 <wanjiaju3108@163.com>
2014-04-15 14:41:12 +0800

    deleted:    ArgFile.cpp 	deleted:    ArgFile.h 	deleted:    Database.cpp 	deleted:    Database.h 	deleted:    Global_Errno.h 	deleted:    ProcDatabase.cpp 	deleted:    ProcDatabase.h 	deleted:    Proc_utils.cpp 	deleted:    Proc_utils.h 	deleted:    Recordset.cpp 	deleted:    Recordset.h 	deleted:    YdDatabase.cpp 	deleted:    YdDatabase.h 	deleted:    asmsgtype.h 	deleted:    astrapmsg.h 	deleted:    header.h 	deleted:    main.cpp 	deleted:    policy_send.cpp 	deleted:    policy_send.h 	deleted:    task_manage.cpp 	deleted:    task_manage.h 	deleted:    task_receive.cpp 	deleted:    task_receive.h 	deleted:    utils.cpp 	deleted:    utils.h

--- policy_send.h
+++
@@ -1,6 +0,0 @@
-#ifndef __POLICY_SEND_H__
-#define __POLICY_SEND_H__
-
-void* policy_send(void *p);
-
-#endif // __POLICY_SEND_H__
--- utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UTIL_H__
-#define __UTIL_H__
-
-int read_n(int fd,  char* buffer, int n);
-int write_n(int fd,  char* buffer, int n);
-
-#endif // __UTIL_H__
-
